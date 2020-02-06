ed1da01e34f33a803cda7a9ec9794230d6e48982
David Lopez <ldavid.x.lopezl@gmail.com>
2020-02-06 15:01:47 -0700

    Add happy/sad paths for all favs endpoint

---
+++ tests/all-playlists.spec.js
@@ -0,0 +1,33 @@
+var request = require("supertest");
+var app = require('../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('Test the favorites endpoints', () => {
+  beforeEach(async () => {
+    await database.raw('truncate table playlists cascade');
+  });
+
+  describe('Test getting all playlists', () => {
+    it('It should respond with all playlists in db', async () => {
+      await database('playlists').insert({title: 'Workout Jams'}, 'id');
+      await database('playlists').insert({title: 'Sad Playlist'}, 'id');
+
+      const res = await request(app)
+        .get("/api/v1/playlists");
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body.length).toBe(2);
+    });
+
+    it('It should respond with a 404 if no favs in db', async () => {
+      const res = await request(app)
+        .get("/api/v1/playlists");
+
+      expect(res.statusCode).toBe(404);
+      expect(res.body).toEqual({ error: "No playlists currently" });
+    });
+  });
+})
