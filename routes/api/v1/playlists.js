107050bee8ec6be42963205bdc3cf66d65ec3b5a
David Lopez <ldavid.x.lopezl@gmail.com>
2020-02-06 15:02:22 -0700

    Add api/v1/playlists get route

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -5,6 +5,16 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
+router.get('/', (req, res) => {
+  database('playlists').select().then(playlists => {
+    if (playlists.length) {
+      res.status(200).json(playlists);
+    } else {
+      res.status(404).json({error: "No playlists currently"});
+    }
+  }).catch(err => res.status(404).json({error: err}))
+});
+
 router.post('/', (req, res) => {
   if (req.body.title) {
     database('playlists').insert({title: req.body.title}, 'id').then(playlist => {
