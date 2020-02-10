7a92cfddd78c264a82c293608d504cb4b441901d
David Lopez <ldavid.x.lopezl@gmail.com>
2020-02-10 13:25:04 -0700

    Abstract out genre happy/sad and rating happy/sad for insert fav method
    Co-Authored-By: Christopher Kelling <cjkelling@users.noreply.github.com>

--- helpers/insert_favorite.js
+++ helpers/insert_favorite.js
@@ -1,22 +1,16 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
+const findGenre = require('./find_genre');
+const findRating = require('./find_rating');
 
 module.exports = async function insertFavorite(data) {
   let trackData = data.message.body.track
-  let genre = null;
-  let rating = trackData.track_rating;
-  if (trackData.primary_genres.music_genre_list[0]) {
-    genre = trackData.primary_genres.music_genre_list[0].music_genre.music_genre_name
-  } else {
-    genre = 'Unknown'
+  let genre = findGenre(data);
+  let rating = findRating(data);
+  if (isNaN(rating) == true) {
+    return rating
   }
-  if (isNaN(rating)) {
-    return {error: "Musixmatch returned a rating that was not an integer"}
-  } else if (rating > 100 || rating < 1) {
-    return {error: "Musixmatch returned a rating that was not in range"}
-  }
-
   return await database('favorites').insert({
     title: trackData.track_name,
     artistName: trackData.artist_name,
