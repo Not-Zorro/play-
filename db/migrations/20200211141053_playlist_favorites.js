efe2d4a158c20c08d0f50f3053a0a6ba698ba3cf
Christopher Kelling <cj.kelling88@gmail.com>
2020-02-12 13:38:07 -0700

    Update database to change favoriteId and playlistId to be favorite_id and playlist_id.
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- db/migrations/20200211141053_playlist_favorites.js
+++ db/migrations/20200211141053_playlist_favorites.js
@@ -1,14 +1,14 @@
 exports.up = function(knex) {
   return knex.schema.createTable('playlist_favorites', function(table) {
     table.increments('id').primary().notNullable();
-    table.integer('playlistId').unsigned().references('id').inTable('playlists').notNullable();
-    table.integer('favoriteId').unsigned().references('id').inTable('favorites').notNullable();
-    table.unique(['playlistId', 'favoriteId'])
+    table.integer('playlist_id').unsigned().references('id').inTable('playlists').notNullable();
+    table.integer('favorite_id').unsigned().references('id').inTable('favorites').notNullable();
+    table.unique(['playlist_id', 'favorite_id'])
   })
 };
 
 exports.down = function(knex) {
   return knex.schema.table('playlist_favorites', function(table) {
-    table.dropUnique(['playlistId', 'favoriteId'])
+    table.dropUnique(['playlist_id', 'favorite_id'])
   }).dropTable('playlist_favorites')
 };
